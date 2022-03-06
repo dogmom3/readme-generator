4707dd621589c05abc73a17ca9ea1e772160de53
dogmom3 <blair.seivers@yahoo.com>
2022-03-06 11:56:28 -0500

    update questions

--- Develop/index.js
+++ Develop/index.js
@@ -6,22 +6,54 @@ const questions = [
   {
     type: 'input',
     name: 'title',
-    message: 'What is the title of your project?'
+    message: 'What is the title of your project? (Required)',
+    validate: userInput => {
+      if (userInput) {
+        return true;
+      } else {
+        console.log('Please enter the title of your project.');
+        return false;
+      }
+    }
   },
   {
     type: 'input',
     name: 'description',
-    message: 'Please give a description of your project.'
+    message: 'Please give a description of your project. (Required)',
+    validate: userInput => {
+      if (userInput) {
+        return true;
+      } else {
+        console.log('Please enter a desciption of your project.');
+        return false;
+      }
+    }
   },
   {
     type: 'input',
     name: 'installation',
-    message: 'What is the installation process for your project?'
+    message: 'What is the installation process for your project? (Required)',
+    validate: userInput => {
+      if (userInput) {
+        return true;
+      } else {
+        console.log('Please enter the installation process for your project.');
+        return false;
+      }
+    }
   },
   {
     type: 'input',
     name: 'usage',
-    message: 'How will you use this project?'
+    message: 'How will you use this project? (Required)',
+    validate: userInput => {
+      if (userInput) {
+        return true;
+      } else {
+        console.log('Please enter details on how to use your project.');
+        return false;
+      }
+    }
   },
   {
     type: 'checkbox',
@@ -32,33 +64,65 @@ const questions = [
   {
     type: 'input',
     name: 'contributers',
-    message: 'Who contributed to this project?'
+    message: 'Who contributed to this project? (Required)',
+    validate: userInput => {
+      if (userInput) {
+        return true;
+      } else {
+        console.log('Please list who contributed to this project.');
+        return false;
+      }
+    }
   },
   {
     type: 'input',
     name: 'tests',
-    message: 'How do you test this project?'
+    message: 'How do you test this project? (Required)',
+    validate: userInput => {
+      if (userInput) {
+        return true;
+      } else {
+        console.log('Please enter the process for testing your project.');
+        return false;
+      }
+    }
   },
   {
     type: 'input',
     name: 'email',
-    message: 'What is your email address?'
+    message: 'What is your email address? (Required)',
+    validate: userInput => {
+      if (userInput) {
+        return true;
+      } else {
+        console.log('Please enter an email address.');
+        return false;
+      }
+    }
   },
   {
     type: 'input',
     name: 'username',
-    message: 'What is your github user name?'
+    message: 'What is your github user name? (Required)',
+    validate: userInput => {
+      if (userInput) {
+        return true;
+      } else {
+        console.log('Please enter your GitHub user name.');
+        return false;
+      }
+    }
   },
 ];
 
-function writeToFile(fileName, data){
+function writeToFile(fileName, data) {
 
-const pageREADME = generatePage(data);
-fs.writeFile(fileName, pageREADME, err => {
-  if (err) throw err;
-  console.log(err);
-console.log('Page complete! Check out readme-generator.md to see the output!');
-});
+  const pageREADME = generatePage(data);
+  fs.writeFile(fileName, pageREADME, err => {
+    if (err) throw err;
+    console.log(err);
+    console.log('Page complete! Check out readme-generator.md to see the output!');
+  });
 }
 
 // TODO: Create a function to initialize app
