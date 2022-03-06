eadbbfadfca7903602fd6698b5686ad9339f79b0
dogmom3 <blair.seivers@yahoo.com>
2022-03-06 11:36:46 -0500

    license links & readme context

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -5,15 +5,15 @@ function renderLicenseBadge(license) {
   // let licenseBadge = [];
   if (license.includes('Apache 2.0 License')) {
     // licenseBadge +=
-      return'[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]';
+    return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]';
   }
   if (license.includes('Boost Software License 1.0')) {
-    return'[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)';
+    return '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)';
   }
   if (license.includes('BSD 3-Clause License')) {
-    return'[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)';
+    return '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)';
   }
-  else { 
+  else {
     return '';
   }
 }
@@ -21,14 +21,14 @@ function renderLicenseBadge(license) {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if (license,includes('Apache 2.0 License')) {
-    return'[![License: ${license}](https://opensource.org/licenses/Apache-2.0)';
+  if (license, includes('Apache 2.0 License')) {
+    return '[![License: ${license}](https://opensource.org/licenses/Apache-2.0)';
   }
   if (license.includes('Boost Software License 1.0')) {
-    return'[![License: ${license}](https://opensource.org/licenses/Boost-Software-1.0)';
+    return '[![License: ${license}](https://boost.org/LICENSE_1_0.txt)';
   }
   if (license.includes('BSD 3-Clause License')) {
-    return'[![License: ${license}](https://opensource.org/licenses/BSD-3-Clause)';
+    return '[![License: ${license}](https://opensource.org/licenses/BSD-3-Clause)';
   }
   else {
     return '';
@@ -38,13 +38,13 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-if (license == 'none') {
-  return '';
-}
-else {
-  return `${renderLicenseLink(license)}
+  if (license == 'none') {
+    return '';
+  }
+  else {
+    return `${renderLicenseLink(license)}
   ${renderLicenseBadge(license)}`;
-}
+  }
 }
 
 
